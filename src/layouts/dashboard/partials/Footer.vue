feef3321b0b0f00df17804f84f84f3bdf7e0daf1
Reda Awwad <aw25re@gmail.com>
2023-06-09 20:40:32 +0300

    fix dark border color in TopHeader Component

--- src/layouts/dashboard/partials/Footer.vue
+++ src/layouts/dashboard/partials/Footer.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
-    import { RouterLink } from 'vue-router';
+import { RouterLink } from 'vue-router';
 
-    const currentYear = new Date().getFullYear();
+const currentYear = new Date().getFullYear();
 </script>
 
 <template>
     <footer
-        class="flex items-center justify-between h-[65px] px-4 px-sm-6 py-6 text-sm border-t-[0.5px] border-s-[0.5px] border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-800 dark:text-white"
-    >
+        class="flex items-center justify-between h-[65px] px-4 px-sm-6 py-6 text-sm bg-white dark:bg-gray-800 dark:text-white">
         <span>Vuetail &copy; {{ currentYear }}</span>
         <ul class="flex items-center gap-x-2">
             <li>
