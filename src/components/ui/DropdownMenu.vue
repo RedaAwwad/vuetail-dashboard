27a9115278c95931b20b4ad1c53476423439255a
Reda Awwad <aw25re@gmail.com>
2023-07-07 13:59:26 +0300

    refactor DropdownMenu component

--- src/components/ui/DropdownMenu.vue
+++ src/components/ui/DropdownMenu.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-    import { ref } from 'vue';
     import { RouterLink } from 'vue-router';
     import type { DropdownMenuItem } from '@/types/ui-components';
 
@@ -15,47 +14,39 @@
         position: 'right-1/2',
     });
 
-    const isMenuOpen = ref<boolean>(false);
-    const menuControl = ref<HTMLElement | null>(null);
-    const menu = ref<HTMLElement | null>(null);
 </script>
 
 <template>
-    <div
-        ref="menuControl"
-        class="relative"
-    >
-        <div class="dropdown dropdown-end">
-            <slot name="control" />
+    <div class="dropdown dropdown-end">
+        <slot name="control" />
 
-            <ul
-                v-if="$slots.default"
-                tabindex="0"
-                class="menu menu-sm dropdown-content rtl:right-auto rtl:left-0 mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-            >
-                <slot />
-            </ul>
+        <ul
+            v-if="$slots.default"
+            tabindex="0"
+            class="menu menu-sm dropdown-content rtl:right-auto rtl:left-0 mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+        >
+            <slot />
+        </ul>
 
-            <ul
-                v-else
-                tabindex="0"
-                class="menu menu-sm dropdown-content rtl:right-auto rtl:left-0 mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+        <ul
+            v-else
+            tabindex="0"
+            class="menu menu-sm dropdown-content rtl:right-auto rtl:left-0 mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+        >
+            <li
+                v-for="(item, index) in menuItems"
+                :key="index"
             >
-                <li
-                    v-for="(item, index) in menuItems"
-                    :key="index"
+                <RouterLink
+                    :to="item.to"
+                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
                 >
-                    <RouterLink
-                        :to="item.to"
-                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                        <div class="inline-flex items-center">
-                            <span>{{ item.label }}</span>
-                        </div>
-                        {{ item.id }}
-                    </RouterLink>
-                </li>
-            </ul>
-        </div>
+                    <div class="inline-flex items-center">
+                        <span>{{ item.label }}</span>
+                    </div>
+                    {{ item.id }}
+                </RouterLink>
+            </li>
+        </ul>
     </div>
 </template>
